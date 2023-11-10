512c399c28cbb85455fadf9d89f8b8253aa0386b
rayy_11 <rayy.11@nycu.edu.tw>
2023-11-10 15:48:35 +0800

    1110_1548

--- main.py
+++ main.py
@@ -14,23 +14,42 @@ class GA_FJSP:
         self.all_op_options = []
         self.populations = []
 
+        self.job_num = 0
+        self.machine_num = 0
+
         # some hyperparameter
         self.population_num = 1
 
         return
 
     def load_instance(self, filename):
-        self.all_op_options = load_instance(filename)
+        self.all_op_options, self.job_num, self.machine_num = load_instance(filename)
 
         for job in self.all_op_options:
             print(job)
 
     def initial_population(self):
-        raise "not yet"
-        random_init()
-    def fitness_evaluation(self, his):
-        raise "not yet"
+        # raise "not yet"
+        self.populations.append(random_init(self.all_op_options, self.job_num))
+    def fitness_evaluation(self, process):
+        # raise "not yet"
+        # make sure the process satisfy the job precedent constrain
+
         # calculate makespan
+        job_current_loading = [0] * self.job_num
+        machine_current_loading = [0] * self.machine_num
+
+        for p in process:
+            job_current_loading[p[0]] = max(job_current_loading[p[0]], machine_current_loading[p[2]]) + self.all_op_options[p[0]][p[1]][p[2]]
+            machine_current_loading[p[2]] = job_current_loading[p[0]]
+            # print('job {} on machine {} with process time : {}'.format(p[0], p[2], self.all_op_options[p[0]][p[1]][p[2]]))
+
+            # print(job_current_loading)
+            # print(machine_current_loading)
+            # print('-'*50)
+        
+
+        return np.max(machine_current_loading)
         
     def selection(self):
         raise "not yet"
@@ -43,6 +62,8 @@ class GA_FJSP:
 def main():
     env = GA_FJSP()
     env.load_instance('./datasets/FJSP/Brandimarte_Data/Mk01.fjs')
+    env.initial_population()
+    env.fitness_evaluation(env.populations[0])
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -3,14 +3,37 @@ import time
 import numpy as np
 from params import get_args
 
-def heuristic_init(all_op_options):
-    job_progress_idx = [0] * len(all_op_options)
-    job_progress_time = [0] * len(all_op_options)
+def random_init(all_op_options, job_num):
+    """
+    process = [(i0, j0, k0), (i1, j1, k1) ... (it, jt, kt) ...]
+    assert len(process) = total operation num
 
-    while True:
+    p-th tuple (ip, jp, kp) means that the jp-th operation of ip-th job executed by kp-th machine
+    """
+    # for job in all_op_options:
+    #     print(job)
+    job_progress = np.zeros(job_num, dtype=int) 
+    finish_job_num = 0
+    process = []
+    while finish_job_num < job_num:
+        next_job = random.choice(np.where(job_progress >= 0)[0])
+        select_m = random.choice(np.where(np.array(all_op_options[next_job])[job_progress[next_job]] > 0)[0])
+        # print('next_job : {}'.format(next_job))
+        # print('its progress : {}'.format(job_progress[next_job]))
+        # print('this job : {}'.format(np.array(all_op_options[next_job])))
+        # print('this op : {}'.format(np.array(all_op_options[next_job])[job_progress[next_job]]))
+        # print('select machine : {}'.format(select_m))
+        # print('-'*50)
+        process.append((next_job, job_progress[next_job], select_m))
 
-        p = random.rand()
-
+        job_progress[next_job] += 1
+        if (job_progress[next_job] >= len(all_op_options[next_job])):
+            job_progress[next_job] = -1
+            finish_job_num += 1
+    # for t in process:
+    #     print('(job_id, op_id, machine_id) : {} process time : {}'.format(t, all_op_options[t[0]][t[1]][t[2]]))
+    # print(len(process))
+    return process
 
 def load_instance(filename):
     all_op_options = []
@@ -34,13 +57,12 @@ def load_instance(filename):
             this_job.append(this_op_options)
         all_op_options.append(this_job)
 
-    for job in all_op_options:
-        print(job)
+    # for job in all_op_options:
+    #     print(job)
 
-    print(len(all_op_options))
-
-    return all_op_options
+    return all_op_options, job_num, machine_num
 
 if __name__ == '__main__':
     args = get_args()
-    load_instance('./datasets/FJSP/Brandimarte_Data/Mk01.fjs')
+    
+    random_init(load_instance('./datasets/FJSP/Brandimarte_Data/Mk01.fjs'))
